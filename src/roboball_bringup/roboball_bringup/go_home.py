901abf9e63727dfe16d5feed237aab0cabc8bcd9
mokingyou <micahmoking@gmail.com>
2026-05-07 13:28:09 -0700

    try rhythmic motion

--- src/roboball_bringup/roboball_bringup/go_home.py
+++ src/roboball_bringup/roboball_bringup/go_home.py
@@ -62,8 +62,8 @@ At time 1778146688.346375015
 HOME_POSITIONS = [
     -2.316411157647604,   # shoulder_lift_joint
     -1.810107946395874,  # elbow_joint
-    1.0502943235584716,   # wrist_1_joint
-    2.0732617378234863,   # wrist_2_joint
+    4.19159265,   # wrist_1_joint
+    5.211,   # wrist_2_joint
     -1.5386202971087855,  # wrist_3_joint
     5.799105644226074,    # shoulder_pan_joint
 ]
